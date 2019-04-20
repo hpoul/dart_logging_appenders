de45af5b974da244bb4505a7a381af5fa819bba9
Herbert Poul <herbert@poul.at>
2019-04-20 15:15:31 +0300

    forgot to export packages.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+## [0.1.0+2] - 2019-04-20 hotfix
 ## [0.1.0+1] - 2019-04-20 broaden dependencies for 0.x versions.
 
 ## [0.1.0] - 2019-04-20 initial release
--- lib/remote_logging_handlers.dart
+++ lib/remote_logging_handlers.dart
@@ -1,7 +1,4 @@
 library remote_logging_handlers;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/logzio_logger.dart';
+export 'src/loki_logger.dart';
