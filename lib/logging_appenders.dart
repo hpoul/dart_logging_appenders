053b9fa9ad21da91e5eaa133210868581eff45b1
Herbert Poul <herbert@poul.at>
2019-08-19 15:26:15 +0200

    do not depend on flutter.

--- lib/logging_appenders.dart
+++ lib/logging_appenders.dart
@@ -3,4 +3,5 @@ library logging_appenders;
 export 'src/logrecord_formatter.dart';
 export 'src/logzio_appender.dart';
 export 'src/loki_appender.dart';
+export 'src/print_appender.dart';
 export 'src/rotating_file_appender.dart';
--- lib/src/print_appender.dart
+++ lib/src/print_appender.dart
@@ -2,6 +2,8 @@ import 'package:logging/logging.dart';
 import 'package:logging_appenders/src/base_appender.dart';
 import 'package:logging_appenders/src/logrecord_formatter.dart';
 
+/// Appender which outputs all log records using the given formatter to
+/// stdout using `print()`.
 class PrintAppender extends BaseLogAppender {
   PrintAppender({LogRecordFormatter formatter}) : super(formatter);
 
