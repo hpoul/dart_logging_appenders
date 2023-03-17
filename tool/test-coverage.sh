4898a9ec5a98446f5508dfc13981b6f89dcc0f56
Herbert Poul <herbert@codeux.design>
2023-03-17 21:04:31 +0100

    fix test coverage.

--- tool/test-coverage.sh
+++ tool/test-coverage.sh
@@ -6,11 +6,16 @@ cd "${0%/*}"/..
 
 
 dart pub get
-dart pub global activate test_coverage
+dart pub global activate coverage
 
 fail=false
-dart pub global run test_coverage --port 38274 || fail=true
+dart test --coverage coverage || fail=true
 echo "fail=$fail"
+
+jq -s '{coverage: [.[].coverage] | flatten}' $(find coverage -name '*.json' | xargs) > coverage/merged_json.cov
+
+dart pub global run coverage:format_coverage -i coverage/merged_json.cov -l --report-on lib --report-on test > coverage/lcov.info
+
 bash <(curl -s https://codecov.io/bash) -f coverage/lcov.info
 
 test "$fail" == "true" && exit 1
